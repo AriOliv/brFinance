58626ba8643eef99e1ffd63145219baf13e045c6
AriOliv <ari.oliveira@protonmail.com>
2024-04-09 23:08:28 -0300

    adicionando parser de data

--- brfinance/utils.py
+++ brfinance/utils.py
@@ -8,6 +8,12 @@ def extract_substring(start, end, string):
 def get_enet_download_url(numSequencia, numVersao, numProtocolo, descTipo):
     return ENET_URL + f"frmDownloadDocumento.aspx?Tela=ext&numSequencia={numSequencia}&numVersao={numVersao}&numProtocolo={numProtocolo}-79&descTipo={descTipo}&CodigoInstituicao=1"
 
+def parse_date(obj):
+    from datetime import datetime
+
+    data_obj = datetime.strptime(obj, '%d/%m/%Y')
+    return data_obj.strftime('%Y-%m-%d')
+
 def parse_stock_amount(reports):
     dict_ = reports.to_dict()
 
@@ -16,9 +22,11 @@ def parse_stock_amount(reports):
             dict_[1][k] = int(dict_[1][k].replace(".",""))
         except:
             pass
+
+
     
     return {
-        "ref_date": dict_[1][0],
+        "ref_date": parse_date(dict_[1][0]),
         "capital_integralizado_on": dict_[1][2],
         "capital_integralizado_pn": dict_[1][3],
         "capital_integralizado_total": dict_[1][4],
